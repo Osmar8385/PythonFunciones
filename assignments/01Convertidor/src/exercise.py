d09fc4e536508a693042b41a28d4a5fe596d7e3c
Osmar8385 <110926409+Osmar8385@users.noreply.github.com>
2022-08-31 20:20:59 -0500

    Update exercise.py

--- assignments/01Convertidor/src/exercise.py
+++ assignments/01Convertidor/src/exercise.py
@@ -2,7 +2,41 @@
 
 def main():
     # Escribe tu código abajo de esta línea
+def Pies_cm(opcion):
+    pies = cantidad * 30.48
+    return pies
+
+def Pulgadas_cm(opcion):
+    pulgadas = cantidad * 2.54
+    return pulgadas
+
+def Yardas_cm(opcion):
+    yardas = cantidad * 90.44
+    return yardas
+
+print('1. pies a cm, 2. pulgadas a cm, 3. yardas a cm')
+
+opcion=int(input('Introduce una opción:'))
+
+cantidad=int(input('Introduce una cantidad:'))
+
+if opcion == 1:
+    print("Son:", Pies_cm(opcion), "pies")
     
+elif opcion == 2:
+    print("Son:", Pulgadas_cm(opcion), "pulgadas")
+    
+elif opcion == 3:
+    print("Son:", Yardas_cm(opcion), "yardas")
+    
+elif opcion != 1:
+    print("Error")
+    
+elif opcion != 2:
+    print("Error")
+    
+elif opcion != 3:
+    print("Error")
 
 if __name__ == '__main__':
     main()
